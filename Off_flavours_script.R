fb904bf986d68f53877c3bec8580507fe94c230d
Neha-Bagri <neha.bagri18@gmail.com>
2022-10-01 16:53:23 +0200

    ggline changed with ggplot + stat_summary

--- Off_flavours_script.R
+++ Off_flavours_script.R
@@ -7,6 +7,8 @@ library(tidyr)
 library(tidyverse)
 library(ggpubr)
 library(purrr)
+library(Rmisc)
+library(dplyr)
 
 ######Import Data frame (Excel)#######
 Off_flavour_Data <- read_excel("Off_flavour_Data.xlsx")
@@ -21,103 +23,84 @@ Off_flavour_Data$Panelist <- as.factor(Off_flavour_Data$Panelist)
 
 ######Data Visualization (1)#######
 
-###660
-Off_flavour_660 <- Off_flavour_Data %>% filter(Dosage == 660) 
-ggline(Off_flavour_660, x="Enzyme", y="Intensity",
-       add = "mean_se",
-       color ="Fat",
-       point.size = 2,
-       facet.by ="Fermentation",
-       plot_type = "p",
-       legend = "right",
-       position = position_dodge(w=0.15))+
-  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))
-       
-
-
 ###220
 Off_flavour_220 <- Off_flavour_Data %>% filter(Dosage == 220)
-ggline(Off_flavour_220, x="Enzyme", y="Intensity",
-       add = "mean_se",
-       color ="Fat",
-       point.size = 2,
-       facet.by ="Fermentation",
-       plot_type = "p",
-       legend = "right",
-       position = position_dodge(w=0.15))+
-  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))
+ggplot(Off_flavour_220,aes(Enzyme,Intensity,colour=Fat))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  facet_grid(cols = vars(Fermentation))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  ylim(-0.1,4.01) +  theme_bw()
 
+###660
+Off_flavour_660 <- Off_flavour_Data %>% filter(Dosage == 660)
+ggplot(Off_flavour_660,aes(Enzyme,Intensity,colour=Fat))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  facet_grid(cols = vars(Fermentation))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  ylim(-0.1,4.01) +  theme_bw()
 
 ######Data Visualization (2)#######
 
 ###BUTTER
 Off_flavour_Butter <- Off_flavour_Data %>% filter(Fat == "Butter")
-ggline(Off_flavour_Butter, x="Enzyme", y="Intensity",
-       add = "mean_se",
-       color ="Dosage",palette = c("#F5AAB0","#D36069"),
-       point.size = 3,
-       shape = 18,
-       facet.by ="Fermentation",
-       plot_type = "p",
-       legend = "right",
-       position = position_dodge(w=0.15))+
+ggplot(Off_flavour_Butter,aes(Enzyme,Intensity,colour=Dosage))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  scale_colour_manual(values=c("#F5AAB0","#D36069"))+
   labs(colour = "Dosage (ALU/kg)")+
-  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))
+  facet_grid(cols = vars(Fermentation))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  ylim(-0.1,4.01) +  theme_bw()
 
 ###Palm
 Off_flavour_Palm <- Off_flavour_Data %>% filter(Fat == "Palm")
-ggline(Off_flavour_Palm, x="Enzyme", y="Intensity",
-       add = "mean_se",
-       color ="Dosage",palette = c("#7BB2C4","#13586F"),
-       point.size = 3,
-       facet.by ="Fermentation",
-       plot_type = "p",
-       legend = "right",
-       position = position_dodge(w=0.15))+
+ggplot(Off_flavour_Palm,aes(Enzyme,Intensity,colour=Dosage))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  scale_colour_manual(values=c("#7BB2C4","#13586F"))+
   labs(colour = "Dosage (ALU/kg)")+
-  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))
+  facet_grid(cols = vars(Fermentation))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  ylim(-0.1,4.01) +  theme_bw()
+
 
 ###Sunflower
 Off_flavour_Sunflower <- Off_flavour_Data %>% filter(Fat == "Sunflower")
-ggline(Off_flavour_Sunflower, x="Enzyme", y="Intensity",
-       add = "mean_se",
-       color ="Dosage",palette = c("#E190E4","#AE18B3"),
-       point.size = 3,
-       shape = 18,
-       facet.by ="Fermentation",
-       plot_type = "p",
-       legend = "right",
-       position = position_dodge(w=0.15))+
+ggplot(Off_flavour_Sunflower,aes(Enzyme,Intensity,colour=Dosage))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  scale_colour_manual(values=c("#E190E4","#AE18B3"))+
   labs(colour = "Dosage (ALU/kg)")+
-  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))
+  facet_grid(cols = vars(Fermentation))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  ylim(-0.1,4.01) +  theme_bw()
 
 ###Coconut
 Off_flavour_Coconut <- Off_flavour_Data %>% filter(Fat == "Coconut")
-ggline(Off_flavour_Coconut, x="Enzyme", y="Intensity",
-       add = "mean_se",
-       color ="Dosage",palette = c("#A6D26D","#45710D"),
-       point.size = 3,
-       shape = 18,
-       facet.by ="Fermentation",
-       plot_type = "p",
-       legend = "right",
-       position = position_dodge(w=0.15))+
+ggplot(Off_flavour_Coconut,aes(Enzyme,Intensity,colour=Dosage))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  scale_colour_manual(values=c("#A6D26D","#45710D"))+
   labs(colour = "Dosage (ALU/kg)")+
-  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))
+  facet_grid(cols = vars(Fermentation))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  ylim(-0.1,4.01) +  theme_bw()
 
 
 
 ######ANOVA & T.test b/w Rounds of Panelists############
 
-Off_flavour_panel <- read_excel("Off_flavour_Data.xlsx", sheet = "Butter_panel_df")
-Off_flavour_panel$Enzyme.2 <- as.factor(Off_flavour_panel$Enzyme.2)
+Off_flavour_panel <- read_excel("Off_flavour_Data.xlsx", sheet = "Palm_panel_220")
+Off_flavour_panel$Enzyme <- as.factor(Off_flavour_panel$Enzyme)
 Off_flavour_panel$Round <- as.factor(Off_flavour_panel$Round)
 
-compare_means(Intensity.avg~Round,
+compare_means(Intensity~Round,
               data=Off_flavour_panel,
               method = "anova")
 
-compare_means(Intensity.avg~Round,
+compare_means(Intensity~Round,
               data=Off_flavour_panel,
               method = "t.test")
 
@@ -164,4 +147,41 @@ compare_means(Intensity.avg~Round,
 compare_means(Intensity.avg~Round,
               data=Off_flavour_panel,
               method = "t.test")
+
+######Data Visualization (3)#######
+
+#Butter_panel
+butter_panel <- read_excel("Off_flavour_Data.xlsx", sheet = "Butter_panel_220")
+
+#strings
+butter_panel$Enzyme <- as.factor(butter_panel$Enzyme)
+butter_panel$Fermentation <- as.factor(butter_panel$Fermentation)
+butter_panel$Intensity <- as.numeric(butter_panel$Intensity)
+butter_panel$Panelist <- as.factor(butter_panel$Panelist)
+
+ggplot(butter_panel,aes(Enzyme,Intensity,colour=Fermentation))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4, shape = 18)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  scale_colour_manual(values=c("#F5AAB0","#D36069"))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  labs(colour = "Fermentation at dosage 220 ALU/kg")+
+  ylim(-0.1,4.01) +  theme_bw()
+
+
+#Palm_panel
+Palm_panel <- read_excel("Off_flavour_Data.xlsx", sheet = "Palm_panel_220")
+
+#strings
+Palm_panel$Enzyme <- as.factor(Palm_panel$Enzyme)
+Palm_panel$Fermentation <- as.factor(Palm_panel$Fermentation)
+Palm_panel$Intensity <- as.numeric(Palm_panel$Intensity)
+Palm_panel$Panelist <- as.factor(Palm_panel$Panelist)
+
+ggplot(Palm_panel,aes(Enzyme,Intensity,colour=Fermentation))+
+  geom_point(stat="summary",fun="mean",position=position_dodge(w=0.2), size = 4, shape = 18)+
+  stat_summary(geom="errorbar",fun.data = mean_se,position=position_dodge(width=0.2),width=0.5)+
+  scale_colour_manual(values=c("#7BB2C4","#13586F"))+
+  scale_x_discrete(limits=c("Ref","A","B","C","D","E"))+
+  labs(colour = "Fermentation at dosage 220 ALU/kg")+
+  ylim(-0.1,4.01) +  theme_bw()
 
